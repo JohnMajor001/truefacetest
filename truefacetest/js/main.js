55a576bc15ff56bf3c67452c0ffbed46c67d6cbb
John Major <johnmajor@Administrators-Mac-mini.local>
2018-03-02 16:08:09 +0000

    carousel has no dot navigation

---
+++ truefacetest/js/main.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+  $('.carousel').slick({
+    slidesToScroll: 1,
+    slidesToShow: 1,
+    autoplay: true,
+    autoplaySpeed: 4000,
+    arrows: false,
+    draggable: false,
+    pauseOnHover: false,
+  //  pauseOnFocus: false,
+  //dots: true,
+    fade: true,
+  });
+});
